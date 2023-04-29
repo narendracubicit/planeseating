33d105d46bf57ad80df070f50abbdf7e4d683161
narendracubicit <pallanarendra3@gmail.com>
2023-04-30 00:38:27 +0530

    plane seating initial commit

---
+++ src/seating/PlaneSeatingFourPerson.java
@@ -0,0 +1,56 @@
+package seating;
+
+import java.util.*;
+
+public class PlaneSeatingFourPerson {
+
+    public static int solution(int n, String s) {
+        int totalNumberOfFamilyOfFours = 0;
+        if (s.isEmpty()) {
+            // if entire row is unreserved, max 2 family can be seated  in this arrangement "CDEF" and "GHJK"
+            totalNumberOfFamilyOfFours = 2 * n;
+            System.out.println("Reserved seats:" + s + ":rows:" + n + "; Total number of family of four that can fit together in unreserved seats:" + totalNumberOfFamilyOfFours);
+            return totalNumberOfFamilyOfFours;
+        }
+        Map<Integer, List<Character>> map = new HashMap<>();
+        for (String occupiedSeat : s.split(" ")) {
+            int row = Integer.parseInt(occupiedSeat.substring(0, occupiedSeat.length() - 1));
+            char columnChar = occupiedSeat.substring(occupiedSeat.length() - 1).charAt(0);
+            map.putIfAbsent(row, new ArrayList<>());
+            map.get(row).add(columnChar); // row as key and list of reserved columns as value
+        }
+        // if entire row is unreserved, max 2 family of 4 can be seated  in this arrangement CDEF and GHJK
+        totalNumberOfFamilyOfFours = 2 * (n - map.size());
+        for (Map.Entry<Integer, List<Character>> entry : map.entrySet()) {
+            List<Character> seats = entry.getValue();
+            boolean isLeftAisle = false, isRightAisle = false, isMiddle = false;
+            for (char seat : seats) {
+                // we can ignore A and K as it's not impacting seating arrangement
+                if (seat >= 'B' && seat <= 'E')
+                    isLeftAisle = true;
+                if (seat >= 'F' && seat <= 'J')
+                    isRightAisle = true;
+                if (seat >= 'D' && seat <= 'G')
+                    isMiddle = true;
+                if (isLeftAisle && isRightAisle && isMiddle) {
+                    // skipping this as this is already covered in "2 * (n-map.size())"
+                    break;
+                }
+            }
+            if (!isLeftAisle)
+                totalNumberOfFamilyOfFours += 1;
+            if (!isRightAisle)
+                totalNumberOfFamilyOfFours += 1;
+            if (isLeftAisle && isRightAisle && !isMiddle)
+                totalNumberOfFamilyOfFours += 1;
+        }
+        System.out.println("Reserved seats:" + s + ":rows:" + n + "; Total number of family of four that can fit together in unreserved seats:" + totalNumberOfFamilyOfFours);
+        return totalNumberOfFamilyOfFours;
+    }
+
+    public static void main(String[] args) {
+        solution(2, "1A 2F 1C");
+        solution(1, "");
+        solution(22, "1A 3C 2B 20G 5A");
+    }
+}
